fcb06de4108dc35c968fd3f6d28c2e91917dadec
Shubzzz <goradesd77@gmail.com>
2026-05-22 23:58:15 +0530

    Update Login.jsx
    qqqqq

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const handleLogin = async () => {
   try {
 
     const response = await fetch(
-      "https://birthday-surprise-for-you.onrender.com/api/login",
+      "https://birthday-surprise-for-you.onrender.com",
       {
         method: "POST",
         headers: {
