b2ac047a00e89c4fa9fe7b14d56ec6a4c0cc076c
Shubzzz <goradesd77@gmail.com>
2026-05-23 00:44:40 +0530

    Update server.js
    333

--- server/server.js
+++ server/server.js
@@ -70,6 +70,6 @@ app.post("/api/login", async (req, res) => {
   });
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0/0", () => {
   console.log(`Server Running on Port ${PORT}`);
 });
